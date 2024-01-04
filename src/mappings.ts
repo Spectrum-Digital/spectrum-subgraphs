8f68ab1dcfdc2de74bbe3a1d3489379db3dffd2f
coinlordd <coinlord.developer@gmail.com>
2024-01-04 14:30:04 +0100

    deploy subgraphs

--- src/mappings.ts
+++ src/mappings.ts
@@ -1,4 +1,4 @@
-import {Address} from '@graphprotocol/graph-ts'
+import {Address, ethereum} from '@graphprotocol/graph-ts'
 
 // Generic schemas across all factories and subgraphs
 import {ERC20} from './generated/AerodromeFactory/ERC20'
@@ -6,22 +6,58 @@ import {Pool, PoolFactory, Token} from './generated/schema'
 
 // Specific events for each factory
 import {PoolCreated as AerodromePoolCreated} from './generated/AerodromeFactory/PoolFactory'
+import {PairCreated as BasedV2PoolCreated} from './generated/BasedV2Factory/PoolFactory'
 import {PairCreated as CamelotPoolCreated} from './generated/CamelotFactory/PoolFactory'
+import {PairCreated as EqualizerV2PoolCreated} from './generated/EqualizerV2Factory/PoolFactory'
+import {PairCreated as EqualizerV3PoolCreated} from './generated/EqualizerV3Factory/PoolFactory'
+import {PairCreated as PancakeswapV2PoolCreated} from './generated/PancakeswapV2Factory/PoolFactory'
+import {PairCreated as RamsesPoolCreated} from './generated/RamsesFactory/PoolFactory'
 import {PairCreated as SpookyswapV2PoolCreated} from './generated/SpookyswapV2Factory/PoolFactory'
+import {PoolCreated as VelodromePoolCreated} from './generated/VelodromeFactory/PoolFactory'
+import {PairCreated as WigoswapV2PoolCreated} from './generated/WigoswapV2Factory/PoolFactory'
 
-export function onAerodromePoolCreated(event: AerodromePoolCreated): void {
-  _onPoolCreated(event.address, event.params.token0, event.params.token1, event.params.pool, event.params.stable)
+export function onAerodromeV2PoolCreated(event: AerodromePoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pool, event.params.stable)
+}
+
+export function onBasedV2PoolCreated(event: BasedV2PoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, false)
 }
 
 export function onCamelotPoolCreated(event: CamelotPoolCreated): void {
-  _onPoolCreated(event.address, event.params.token0, event.params.token1, event.params.pair, false)
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, false)
+}
+
+export function onEqualizerV2PoolCreated(event: EqualizerV2PoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, event.params.stable)
+}
+
+export function onEqualizerV3PoolCreated(event: EqualizerV3PoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, event.params.stable)
+}
+
+export function onPancakeswapV2PoolCreated(event: PancakeswapV2PoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, false)
+}
+
+export function onRamsesPoolCreated(event: RamsesPoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, event.params.stable)
 }
 
 export function onSpookyswapV2PoolCreated(event: SpookyswapV2PoolCreated): void {
-  _onPoolCreated(event.address, event.params.token0, event.params.token1, event.params.pair, false)
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, false)
+}
+
+export function onVelodromeV2PoolCreated(event: VelodromePoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pool, event.params.stable)
+}
+
+export function onWigoswapV2PoolCreated(event: WigoswapV2PoolCreated): void {
+  _onPoolCreated(event, event.address, event.params.token0, event.params.token1, event.params.pair, false)
 }
 
 export function _onPoolCreated(
+  event: ethereum.Event,
   factory: Address,
   token0Address: Address,
   token1Address: Address,
@@ -37,6 +73,7 @@ export function _onPoolCreated(
 
   // Create the pool
   const pool = new Pool(poolAddress.toHexString())
+  pool.blockNumber = event.block.number
   pool.factory = factory.toHexString()
   pool.decimals = fetchTokenDecimals(poolAddress)
   pool.token0 = token0.id
